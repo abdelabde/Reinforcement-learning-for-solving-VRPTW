0d661117d4b71a96658801931b90c5621432a230
AbdelAbdel <45332749+abdelabde@users.noreply.github.com>
2021-04-06 11:17:12 +0100

    Update enviroment.py

--- RL4VRPTW/enviroment.py
+++ RL4VRPTW/enviroment.py
@@ -11,13 +11,14 @@ class AgentVRP():
         loc = input[1]
 
         self.batch_size, self.n_loc, _ = loc.shape  # (batch_size, n_nodes, 2)
-
         # Coordinates of depot + other nodes
         self.coords = tf.concat((depot[:, None, :], loc), -2)
         self.demand = tf.cast(input[2], tf.float32)
-        self.time_windows=tf.cast(input[3], tf.string)
-        self.time_windows=tf.concat([tf.zeros((self.time_windows.shape[0],1), dtype=tf.string),self.time_windows],1)
-        self.service_time=tf.cast(input[4], tf.float32)
+        self.time_windows_min=tf.cast(input[3], tf.float32)
+        self.time_windows_min=tf.concat([tf.zeros((self.time_windows_min.shape[0],1), dtype=tf.float32),self.time_windows_min],1)
+        self.time_windows_max=tf.cast(input[4], tf.float32)
+        self.time_windows_max=tf.concat([10*tf.ones((self.time_windows_max.shape[0],1), dtype=tf.float32),self.time_windows_max],1)
+        self.service_time=tf.cast(input[5], tf.float32)
         self.service_time=tf.concat([tf.zeros((self.service_time.shape[0],1), dtype=tf.float32),self.service_time],1)
         # Indices of graphs in batch
         self.ids = tf.range(self.batch_size, dtype=tf.int64)[:, None]
@@ -73,8 +74,6 @@ class AgentVRP():
         """Checks if partial solution for all graphs has been built, i.e. all agents came back to depot
         """
         return tf.reduce_all(self.from_depot) and self.i != 0
-    def distance(x,y):
-        return ok
 
     def get_mask(self):
         """ Returns a mask (batch_size, 1, n_nodes) with available actions.
@@ -83,7 +82,6 @@ class AgentVRP():
 
         # Exclude depot
         visited_loc = self.visited[:, :, 1:]
-        
         # Mark nodes which exceed vehicle capacity
         exceeds_cap = self.demand + self.used_capacity > self.VEHICLE_CAPACITY
         # Mark nodes out time windows
@@ -92,20 +90,24 @@ class AgentVRP():
         if self.i>0:
             for i in range(self.coords.shape[1]):
                 var=tf.concat([d,tf.reshape(self.coords[:,i],(self.batch_size,1,2))], 1)
-                temp_dist=tf.math.scalar_mul(2,tf.reduce_sum(tf.norm(var[:, 1:] - var[:, :-1], ord=2, axis=2), axis=1),
-                                             name=None)
+                #temp_dist=tf.math.scalar_mul(2,tf.reduce_sum(tf.norm(var[:, 1:] - var[:, :-1], ord=2, axis=2), axis=1),name=None)
+                temp_dist=tf.norm(var[:, 1:] - var[:, :-1], ord=2, axis=2)
                 temp_service_time=tf.reshape(self.service_time[:,i],(self.batch_size,1))#???????
-                temp_time=tf.add(tf.reshape(temp_dist,(self.batch_size,1)),self.temp_tour_length,temp_service_time)
-                temp_time_windows=self.time_windows
+                temp_time=0.1*tf.reshape(temp_dist,(self.batch_size,1))+self.temp_tour_length+temp_service_time
+                temp_time_windows_min=self.time_windows_min
+                temp_time_windows_max=self.time_windows_max
+                #print(self.time_windows_min)
                 L=[]
                 for j in range(self.batch_size):
-                    a=temp_time_windows[j][i].numpy()
-                    if len(a)>=1:
-                        min_t=int(a[1:2])
-                        max_t=int(a[4:5])
-                        L.append(not ((temp_time[j,:].numpy()[0]>=min_t ) & (max_t>= temp_time[j,:].numpy()[0])))
+                    a=temp_time_windows_min[j][i].numpy()
+                    b=temp_time_windows_max[j][i].numpy()
+                    L.append(not ((temp_time[j,:].numpy()[0]>=a ) & (b >= temp_time[j,:].numpy()[0])))
+                    """if a and b:
+                        #min_t=int(a[1:2])
+                        #max_t=int(a[4:5])
+                        L.append(not ((temp_time[j,:].numpy()[0]>=a ) & (b >= temp_time[j,:].numpy()[0])))
                     else:
-                        L.append(False)
+                        L.append(False)"""
                 exceeds_time_windows=tf.concat([exceeds_time_windows,tf.reshape(tf.convert_to_tensor(np.array(L), dtype=tf.bool, dtype_hint=None, name=None),(self.batch_size,1))], 1)
                 #Verify True or False
             exceeds_time_windows=exceeds_time_windows[:,2:] ###Excluse also depot ??? le contraire of true
@@ -124,12 +126,22 @@ class AgentVRP():
         # Update current state
         selected = action[:, None]
         if self.i  > 0:
-            pi=tf.concat([self.prev_a,selected], 1)
-            #print(self.get_route_length(pi))
-            travel_time=tf.reshape(tf.math.scalar_mul(2, self.get_route_length(pi), name=None),(self.batch_size,1))
+            at=tf.gather(self.coords,tf.cast(self.prev_a, tf.int32), batch_dims=1)
+            br=tf.gather(self.coords,tf.cast(selected, tf.int32), batch_dims=1)
+            pi=tf.concat([tf.reshape(at,(self.batch_size,1,2)),tf.reshape(br,(self.batch_size,1,2))], 1)
+            #print(tf.shape(pi))
+            travel_time=0.1*tf.norm(pi[:, 1:] - pi[:, :-1], ord=2, axis=2)
             d =tf.gather(self.service_time, tf.cast(selected, tf.int32), batch_dims=1)
-            self.temp_tour_length =tf.add(d,travel_time)
+            self.temp_tour_length =self.temp_tour_length+d+travel_time
             #print(self.temp_tour_length)
+        else:
+            br=tf.gather(self.coords,tf.cast(selected, tf.int32), batch_dims=1)
+            at=self.coords[:,0, :]
+            pi=tf.concat([tf.reshape(at,(self.batch_size,1,2)),tf.reshape(br,(self.batch_size,1,2))], 1)
+            #print(tf.shape(pi))
+            travel_time=tf.norm(pi[:, 1:] - pi[:, :-1], ord=2, axis=2)
+            d =0.1*tf.gather(self.service_time, tf.cast(selected, tf.int32), batch_dims=1)
+            self.temp_tour_length =self.temp_tour_length+d+travel_time
         self.prev_a = selected
         self.from_depot = self.prev_a == 0
         # We have to shift indices by 1 since demand doesn't include depot
@@ -144,6 +156,7 @@ class AgentVRP():
         # Update visited nodes (set 1 to visited nodes)
         idx = tf.cast(tf.concat((self.ids, self.scatter_zeros, self.prev_a), axis=-1), tf.int32)[:, None, :]  # (batch_size, 1, 3)
         self.visited = tf.tensor_scatter_nd_update(self.visited, idx, self.step_updates)  # (batch_size, 1, n_nodes)
+        
 
         self.i = self.i + 1
 
@@ -165,7 +178,4 @@ class AgentVRP():
         d = tf.gather(self.coords, tf.cast(pi, tf.int32), batch_dims=1)
 
         # Calculation of total distance
-        # Note: first element of pi is not depot, but the first selected node in the path
-        return (tf.reduce_sum(tf.norm(d[:, 1:] - d[:, :-1], ord=2, axis=2), axis=1)
-                )
-####+ tf.norm(d[:, 0] - self.coords[0], ord=2, axis=1) A ne pas oublier 
+        return (tf.norm(d[:, 1:] - d[:, :-1], ord=2, axis=2))
